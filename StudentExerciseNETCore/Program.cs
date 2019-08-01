eaa2f9ccc660d0b9832d40445693c08745b108de
joab sigrah <jksiggy@gmail.com>
2019-08-01 02:28:11 -0500

    StudentExerciseNETCore

---
+++ StudentExerciseNETCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StudentExerciseNETCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
